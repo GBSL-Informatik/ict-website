e44fd1a53b7b6647ebb77ec173211121e3b623f4
Balthasar Hofer <lebalz@outlook.com>
2022-09-05 00:19:54 +0200

    fix broken Features Category

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.scss';
-import {useCurrentSidebarCategory} from '@docusaurus/theme-common';
+import { useCurrentSidebarCategory } from '@docusaurus/theme-common';
+// @ts-ignore
+import { useDocsSidebar, useSidebarBreadcrumbs } from '@docusaurus/theme-common/internal';
 import Feature from '../Feature';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'; // @ts-ignore
+import { useDoc } from '@docusaurus/theme-common/internal';
+import { useLocation } from '@docusaurus/router';
+// https://github.com/facebook/docusaurus/blob/main/packages/docusaurus-theme-common/src/contexts/docsSidebar.tsx
+import type { PropSidebar } from '@docusaurus/plugin-content-docs';
+type ContextValue = { name: string; items: PropSidebar };
 
-export default function Features(): JSX.Element {
+function MyFeature(): JSX.Element {
   const sidebar = useCurrentSidebarCategory();
   return (
     <div className={styles.features}>
@@ -12,11 +20,26 @@ export default function Features(): JSX.Element {
         if ((item.type === 'link' || item.type === 'category') && item.customProps && item.customProps.icon) {
           return (
             <Feature icon={item.customProps.icon as string} name={item.label} route={item.href} key={idx} />
-          )
+          );
         } else {
           return null;
         }
       })}
     </div>
   );
+}
+
+export default function Features(): JSX.Element {
+  const docsSidebar: ContextValue = useDocsSidebar();
+  const { pathname } = useLocation();
+  const mine = (docsSidebar?.items || []).find((it) => it.type !== 'html' && it.href === pathname);
+  console.log('mine', docsSidebar);
+  if (!mine || mine.type !== 'category') {
+    return (
+      <div className={styles.features}>
+        <Feature icon="mdi-heart-broken" name="Keine Unterseite Gefunden 😢" route={(docsSidebar?.items.find((it) => it.type !== 'html') as {href: string})?.href || '/'} />
+      </div>
+    );
+  }
+  return <MyFeature />;
 }
